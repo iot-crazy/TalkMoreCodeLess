1b8a21ad32885e6378fbe6ffc703894d9fd362fd
iot-crazy <anthonykenny84@gmail.com>
2021-06-30 21:57:11 +0200

    Created basic CLI search

---
+++ TalkMore/DataModels/Camera.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DataModels
+{
+    public class Camera
+    {
+        private static Regex regx = new Regex("(\\d{3})");
+
+        public int ID { get; }
+        public string Name { get; }
+        public float Lat { get; }
+        public float Lng { get; }
+
+        public Camera(int id, string name, float lat, float lng)
+        {
+            ID = id;
+            Name = name;
+            Lat = lat;
+            Lng = lng;
+        }
+
+        public static Camera FromLine(string[] fields)
+        {
+            return new Camera(
+                int.Parse(regx.Match(fields[0]).Value),
+                fields[0],
+                float.Parse(fields[1]),
+                float.Parse(fields[2])
+                );
+        }
+    }
+}
---
+++ TalkMore/Search/Program.cs
@@ -0,0 +1,64 @@
+﻿using DataModels;
+using System;
+using System.IO;
+
+namespace Search
+{
+    class Program
+    {
+        const string DataFile = "./cameras-defb.csv";
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Error: Please specify a search parameter.");
+                DisplayUsage();
+                return;
+            }
+
+            string columnName = args[0].Replace("--", "");
+            string searchValue = args[1];
+
+            var stream = new StreamReader("./Data/cameras-defb.csv");
+            stream.ReadLine(); // skip header line
+
+            while (stream.EndOfStream == false )
+            {
+                string line = stream.ReadLine();
+                MatchLine(line, columnName, searchValue);
+            }
+            
+        }
+
+        static void MatchLine(string line, string columnName, string searchValue)
+        {
+            var fields = line.Split(';');
+            if (fields.Length != 3)
+            {
+                return;
+            }
+
+            var camera = Camera.FromLine(fields);
+
+            if (camera.Name.Contains(searchValue, StringComparison.InvariantCultureIgnoreCase))
+            {
+                DisplayCamera(camera);
+            }
+
+        }
+
+        static void DisplayCamera(Camera camera)
+        {
+            Console.WriteLine($"{camera.ID} | {camera.Name} {camera.Lat} | {camera.Lng}");
+        }
+
+        static void DisplayUsage()
+        {
+            Console.WriteLine("Search Usage:");
+            Console.WriteLine("search --ColumnName --Value");
+            Console.WriteLine("Example  search --name Neude");
+        }
+
+    }
+}
